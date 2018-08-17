f0efd48b0893f5d7dd439d647cad824149df5f1d
effone <effone@mybb.com>
2018-08-17 10:47:33 +0530

    DI Compatibility

--- app/core/func/site.php
+++ app/core/func/site.php
@@ -14,8 +14,15 @@
 
 if (!defined("APP")) die(); // Deny direct access
 
-// Return preconfigured system paths
-// =========================================================================================================
+// Class autoloader
+spl_autoload_register(function($c){@include syspath('class').preg_replace('#\\\|_(?!.+\\\)#','/',$c).'.php';});
+
+/**
+ * Return preconfigured system paths
+ *
+ * @param [type] $target
+ * @return string
+ */
 function syspath($target){
 	$path = parse_ini_file(dirname(__DIR__).'/conf/syspath.ini',true);
 	foreach ($path as $key => $group) {
@@ -27,9 +34,15 @@ function syspath($target){
 	return false;
 }
 
-// Fetch Settings << Fetch user settings as well, avoid hardcoded ini path
-// =========================================================================================================
-function conf($node, $group = 'site'){
+/**
+ * Return Settings Values
+ *
+ * @param [type] $node // Settings name
+ * @param string $group // Settings Group Name
+ * @return mixed
+ * @todo Fetch user settings as well, avoid hardcoded ini path
+ */
+function conf(string $node, string $group = 'site'){
 	$settings = parse_ini_file(syspath('config').'basefact.ini',true);
 	if(!empty($settings[$group][$node])) return $settings[$group][$node];
 	return false;
